68fe6e403a4ec5fdf00d194a47ee5506b628c819
Sahil Shenoy <sahilshenoy3@gmail.com>
2024-09-29 16:47:14 +0530

    link fixing

--- src/app/api/resume.ts
+++ src/app/api/resume.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import AWS from 'aws-sdk';
 
 // Configure AWS SDK
@@ -7,21 +7,25 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-east-1', // Change this to your region
 });
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const params = {
-      Bucket: 'our-resumes', // Replace with your S3 bucket name
-      Key: 'SahilResume.pdf', // Replace with the name of the file in S3
-    };
-  
-    try {
-      // Fetch the resume from S3
-      const data = await s3.getObject(params).promise();
-  
-      // Set the content type and send the PDF file
-      res.setHeader('Content-Type', 'application/pdf');
-      res.send(data.Body);
-    } catch (error) {
-      console.error('Error fetching resume from S3:', error);
-      res.status(500).json({ error: 'Failed to load resume' });
-      }
-    }
+
+export async function GET() {
+  const params = {
+    Bucket: 'our-resumes', // Replace with your S3 bucket name
+    Key: 'SahilResume.pdf', // Replace with the name of the file in S3
+  };
+
+  try {
+    // Fetch the resume from S3
+    const data = await s3.getObject(params).promise();
+
+    // Return the PDF content as a response
+    return new NextResponse(data.Body, {
+      headers: {
+        'Content-Type': 'application/pdf',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching resume from S3:', error);
+    return NextResponse.json({ error: 'Failed to load resume' }, { status: 500 });
+  }
+}
